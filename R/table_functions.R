eeffe4af408f502b5e429ead94f5b8352de82fef
Niklas Knecht <knecht.n@gmx.de>
2022-06-19 13:21:17 +0200

    bugfix

--- R/table_functions.R
+++ R/table_functions.R
@@ -5,39 +5,41 @@ Performance_Summary <- function(df, from){
     EpsilonUtility::transformHLOC(DF=.,
                                   PCT=c(1)) %>%
     filter(date >= as.Date(from)) %>%
+    mutate(range = (high - low) / close) %>%
     mutate(ret = PCT1)
   table <- data.frame(matrix(ncol = 5, nrow=length(symbols)))
   for(k in 1:length(symbols)){
+
     table[k,1] <- df %>%
       filter(symbol == symbols[k]) %>%
       pull(ret) %>%
       cumRet() %>%
-      tail(.,1) %>%
-      round(., 3)
+      tail(.,1)
+
     table[k,1] <- (table[k,1] - 1) %>% round(., 3)
 
     table[k,2] <- df %>%
       filter(symbol == symbols[k]) %>%
       pull(ret) %>%
-      mean(., na.rm=T) %>%
-      round(., 3)
+      mean(., na.rm=T)
 
     table[k,3] <- df %>%
       filter(symbol == symbols[k]) %>%
       pull(ret) %>%
-      sd(., na.rm=T) %>%
-      round(., 3)
+      sd(., na.rm=T)
 
-    table[k,4] <- (((1+table[k,2])^(252)-1) / (table[k,3] * sqrt(252))) %>%
-      round(., 3)
+    table[k,4] <- (((1+table[k,2])^(252)-1) / (table[k,3] * sqrt(252)))
 
     table[k,5] <- df %>%
       filter(symbol == symbols[k]) %>%
       pull(range) %>%
-      mean(., na.rm=T) %>%
-      round(., 3)
+      mean(., na.rm=T)
   }
+  table <- table * 100
+  table <- table %>% round(.,2)
+  table[,4] <- table[,4] / 100
+
   rownames(table) <- symbols
   colnames(table) <- c("Return (cum)", "Return (mean)", "Volatility", "Sharpe Ratio", "Daily Range (mean)")
-  kable(table)
+  knitr::kable(table)
 }
